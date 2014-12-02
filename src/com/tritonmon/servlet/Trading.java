292c4286e133603fe6dd8ab1e2fa038abfaa8201
pksuira <akso@ucsd.edu>
2014-12-01 20:32:34 -0800

    hihi

--- src/com/tritonmon/servlet/Trading.java
+++ src/com/tritonmon/servlet/Trading.java
@@ -49,93 +49,93 @@ public class Trading {
 //		return ServletUtil.getJSON(query);
 //	}
 	
-	// username1 is ALWAYS challenger, username2 is always the challenged
+	// username1 is ALWAYS offerer, username2 is always the lister
 	
 	
 	// initiates username1 challenging username2
 	@POST
-	@Path("/challenge/{username1}/{username2}")
-	public Response challenge(@PathParam("username1") String username1, @PathParam("username2") String username2) {
-		String query = "INSERT INTO challenges (challenger, challenged) VALUES("+ServletUtil.decodeWrap(username1)+","
+	@Path("/trade/{username1}/{username2}")
+	public Response trade(@PathParam("username1") String username1, @PathParam("username2") String username2) {
+		String query = "INSERT INTO trades (offerer, lister) VALUES("+ServletUtil.decodeWrap(username1)+","
 			+ ServletUtil.wrapInString(username2)+");";
 		return ServletUtil.buildResponse(query);
 	}
 	
 	// maybe dont allow this idk
-	// username1 unchallenges username2
+	// username1 untrades username2
 	@POST
-	@Path("/unchallenge/{username1}/{username2}")
-	public Response unchallenge(@PathParam("username1") String username1, @PathParam("username2") String username2) {
-		String query = "DELETE FROM challenges WHERE challenger="+ServletUtil.decodeWrap(username1)+" AND challenged="
+	@Path("/untrade/{username1}/{username2}")
+	public Response untrade(@PathParam("username1") String username1, @PathParam("username2") String username2) {
+		String query = "DELETE FROM trades WHERE offerer="+ServletUtil.decodeWrap(username1)+" AND lister="
 			+ ServletUtil.wrapInString(username2)+";";
 		return ServletUtil.buildResponse(query);
 	}
 	
 	// maybe get them even if declined?
-	// get all not declined challengers
+	// get all not declined offerers
 	@GET
-	@Path("/getchallengers/{username2}")
-	public String getChallengers(@PathParam("username2") String username2) {
-		String query = "SELECT challenger FROM challenges WHERE challenged="+ServletUtil.decodeWrap(username2)+" AND declined=0;";
-		return ServletUtil.parseToList(query, "challenger");
+	@Path("/getofferers/{username2}")
+	public String getofferers(@PathParam("username2") String username2) {
+		String query = "SELECT offerer FROM trades WHERE lister="+ServletUtil.decodeWrap(username2)+" AND declined=0;";
+		return ServletUtil.parseToList(query, "offerer");
 	}
 	
 	// maybe get them even if declined?
-	// get all people you've challenged
+	// get all people you've lister
 	@GET
 	@Path("/getchallengings/{username1}")
 	public String getChallengings(@PathParam("username1") String username1) {
-		String query = "SELECT challenged FROM challenges WHERE challenger="+ServletUtil.decodeWrap(username1)+" AND declined=0;";
-		return ServletUtil.parseToList(query, "challenged");
+		String query = "SELECT lister FROM trades WHERE offerer="+ServletUtil.decodeWrap(username1)+" AND declined=0;";
+		return ServletUtil.parseToList(query, "lister");
 	}
 	
-	// get all unseen challengers
+	// get all unseen offerers
 	@GET
-	@Path("/getunseenchallengers/{username2}")
-	public String getUnseenChallengers(@PathParam("username2") String username2) {
-		String query = "SELECT challenger FROM challenges WHERE challenged="+ServletUtil.decodeWrap(username2)+" AND seen_challenge=0;";
-		return ServletUtil.parseToList(query, "challenger");
+	@Path("/getunseenofferers/{username2}")
+	public String getUnseenofferers(@PathParam("username2") String username2) {
+		String query = "SELECT offerer FROM trades WHERE lister="+ServletUtil.decodeWrap(username2)+" AND seen_offer=0;";
+		return ServletUtil.parseToList(query, "offerer");
 	}
 	
-	// get all unseen declined challenges
+	// get all unseen declined trades
 	@GET
-	@Path("/getunseendeclinedchallengers/{username1}")
-	public String getUnseenDeclinedChallengers(@PathParam("username1") String username1) {
-		String query = "SELECT challenged FROM challenges WHERE challenger="+ServletUtil.decodeWrap(username1)+" AND declined=1 AND seen_decline=0;";
-		return ServletUtil.parseToList(query, "challenged");
+	@Path("/getunseendeclinedofferers/{username1}")
+	public String getUnseenDeclinedofferers(@PathParam("username1") String username1) {
+		String query = "SELECT lister FROM trades WHERE offerer="+ServletUtil.decodeWrap(username1)+" AND declined=1 AND seen_decline=0;";
+		return ServletUtil.parseToList(query, "lister");
 	}
 	
-	// set a challenge to seen (but not decided)
+	// set a trade to seen (but not decided)
 	@POST
-	@Path("/setseenchallenge/{username1}/{username2}")
-	public Response setSeenChallenge(@PathParam("username1") String username1, @PathParam("username2") String username2) {
-		String query = "UPDATE challenges SET seen_challenge=1 WHERE challenger="+ServletUtil.wrapInString(username1)
-				+" AND challenged=" + ServletUtil.wrapInString(username2)+";";
+	@Path("/setseentrade/{username1}/{username2}")
+	public Response setSeenTrade(@PathParam("username1") String username1, @PathParam("username2") String username2) {
+		String query = "UPDATE trades SET seen_offer=1 WHERE offerer="+ServletUtil.wrapInString(username1)
+				+" AND lister=" + ServletUtil.wrapInString(username2)+";";
 		return ServletUtil.buildResponse(query);
 	}
 	
-	// set a challenge to be declined (but not seen the decline yet)
+	// set a trade to be declined (but not seen the decline yet)
 	@POST
-	@Path("/setdeclinechallenge/{username1}/{username2}")
-	public Response setDeclineChallenge(@PathParam("username1") String username1, @PathParam("username2") String username2) {
-		String query = "UPDATE challenges SET seen_challenge=1,declined=1 WHERE challenger="+ServletUtil.wrapInString(username1)
-				+" AND challenged=" + ServletUtil.wrapInString(username2)+";";
+	@Path("/setdeclinetrade/{username1}/{username2}")
+	public Response setDeclineTrade(@PathParam("username1") String username1, @PathParam("username2") String username2) {
+		String query = "UPDATE trades SET seen_offer=1,declined=1 WHERE offerer="+ServletUtil.wrapInString(username1)
+				+" AND lister=" + ServletUtil.wrapInString(username2)+";";
 		return ServletUtil.buildResponse(query);
 	}
 	
-	// sets all seen decline challenges to seen decline
+	// sets all seen decline trades to seen decline
 	@POST
-	@Path("/setdeclinedchallenge/{username1}")
-	public Response setDeclineChallenges(@PathParam("username1") String username1) {
-		String query = "UPDATE challenges SET seen_decline=1 WHERE challenger="+ServletUtil.wrapInString(username1)
+	@Path("/setdeclinedtrade/{username1}")
+	public Response setDeclineTrades(@PathParam("username1") String username1) {
+		String query = "UPDATE trades SET seen_decline=1 WHERE offerer="+ServletUtil.wrapInString(username1)
 				+" AND declined=1 AND seen_decline=0 ;";
 		return ServletUtil.buildResponse(query);
 	}
 	
 	@POST
-	@Path("/removeseendeclinedchallenges")
-	public Response removeSeenDeclinedChallenges() {
-		String query = "DELETE FROM challenges WHERE declined=1 and seen_decline=1;";
+	@Path("/removeseendeclinedtrades")
+	public Response removeSeenDeclinedTrades() {
+		String query = "DELETE FROM trades WHERE declined=1 and seen_decline=1;";
 		return ServletUtil.buildResponse(query);
 	}
 }

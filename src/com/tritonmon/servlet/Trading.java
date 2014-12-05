07543a90bcb881024947a8924b1838f42e4616a6
pksuira <akso@ucsd.edu>
2014-12-04 17:20:32 -0800

    hihi

--- src/com/tritonmon/servlet/Trading.java
+++ src/com/tritonmon/servlet/Trading.java
@@ -95,15 +95,15 @@ public class Trading {
 			@PathParam("pokemon1") String pokemon1,
 			@PathParam("users_id2") String users_id2,
 			@PathParam("pokemon2") String pokemon2) {
-
+//		System.out.println("bobba: " + users_id1 + " , chocho: " + users_id2);
 		// shitty hacky way but too bad our demo is in a week
 		String query = "SELECT slot_num FROM users_pokemon WHERE users_pokemon_id="
-				+ pokemon1;
+				+ pokemon1+";";
 		String result = ServletUtil.getJSON(query);
 		String firstSlotId = result.substring(result.indexOf(":") + 1,
 				result.indexOf("}"));
 		query = "SELECT slot_num FROM users_pokemon WHERE users_pokemon_id="
-				+ pokemon2;
+				+ pokemon2+";";
 		result = ServletUtil.getJSON(query);
 		String secondSlotId = result.substring(result.indexOf(":") + 1,
 				result.indexOf("}"));
@@ -124,7 +124,7 @@ public class Trading {
 				+ users_id1
 				+ " AND lister_users_id="
 				+ users_id2
-				+ "AND offer_users_pokemon_id="
+				+ " AND offer_users_pokemon_id="
 				+ pokemon1
 				+ " AND lister_users_pokemon_id=" + pokemon2 + ";";
 		return ServletUtil.buildResponse(query);
